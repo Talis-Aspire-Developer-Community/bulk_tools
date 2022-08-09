bae3e9d426d803c34a67529caff051ba6bc75f3d
Tim Hodson <tim@timhodson.com>
2022-08-09 11:04:06 +0100

    Update isbn_updater/src/comp.php
    Co-authored-by: lordtatty <lordtatty@googlemail.com>

--- isbn_updater/src/comp.php
+++ isbn_updater/src/comp.php
@@ -15,7 +15,7 @@ $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
 echo '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-	echo "File is valid, and was successfully uploaded.\n";
+	echo "File was successfully uploaded.\n";
 } else {
 	echo "File is invalid, and failed to upload - Please try again. -\n";
 }
